49da8215625c6ac92b9d64e7965a55fc5398c634
David Naranjo <dnaranjo89@gmail.com>
2025-06-11 08:30:44 +0200

    new dist

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/home" element={<App />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
